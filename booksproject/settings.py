8956e5f9bdcfd72340c8b93b675b2b8218c7932d
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2021-01-11 20:15:47 +0200

    done

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -78,11 +78,13 @@ WSGI_APPLICATION = 'booksproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -124,6 +126,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
 'DEFAULT_PERMISSION_CLASSES' : [
-'rest_framework.permissions.AllowAny',
+'rest_framework.permissions.IsAuthenticated',
     ]
 }
