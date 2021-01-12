7d84f7361170763b0bd4d40ca20319e640b0ef26
RaniaAbdullahh <raniaabdullah6600@gmail.com>
2021-01-12 22:19:59 +0200

    addtoken

--- books/views.py
+++ books/views.py
@@ -3,13 +3,14 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveDestro
 from .permission import permission
 from .serializer import booksSerializer
 from .models import Books
+from rest_framework.permissions import IsAuthenticated
 
 class BooksListView(ListAPIView):
     queryset = Books.objects.all()
     serializer_class = booksSerializer
-    permissions_classes = (permission,)
+    permissions_classes = (permission,IsAuthenticated)
 
 class BooksDetailsView(RetrieveUpdateDestroyAPIView):
     queryset = Books.objects.all()
     serializer_class = booksSerializer
-    permissions_classes = (permission,)
+    permissions_classes = (permission,IsAuthenticated)
